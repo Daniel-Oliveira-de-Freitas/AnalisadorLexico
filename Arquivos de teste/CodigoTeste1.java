86002ccbda5c1cceda3a3548cd48da55738e32e7
CassioJr <cassiojuniorceolin@gmail.com>
2021-09-27 16:28:34 -0300

    Adicionado os arquivos de teste

---
+++ Arquivos de teste/CodigoTeste1.java
@@ -0,0 +1,16 @@
+public class CodigoTeste1{
+
+ int num;
+ boolean char;
+ float real;
+ String texto;
+
+ public static void main (String args[]){
+ num =1;
+ if(num == 1)
+ System.out.print("Hello");
+ else 
+ System.out.print("World");
+ }
+
+ }
---
+++ Arquivos de teste/CodigoTeste2.java
@@ -0,0 +1,13 @@
+public class CodigoTeste2{
+int num;
+boolean boleano;
+float real;
+
+public static void main (String args[]){
+num =1;
+for ( boleano == true)
+ num = 1+1;
+}
+
+}
+}
